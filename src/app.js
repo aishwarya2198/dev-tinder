f846a27c5f2028624aa7c013345d64f05d97d110
aishwarya2198 <nuwal.aishwarya06@gmail.com>
2024-10-27 00:21:54 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -5,17 +5,23 @@ const app = express();
 
 // create request handlers => has route information along with req and res. 
 // app.use() will match all API calls to rote so best practice is app.HTTPMethod()
-app.get("/test", (req, res) => {
-    res.send("GET call")
-})
-app.post("/test", (req, res) => {
-    res.send("POST call")
-})
-app.delete("/test", (req, res) => {
-    res.send("DELETE call")
-})
-app.use("/test", (req, res) => {
-    res.send("Hello World from Test");
+app.get("/user", (req, res) => {
+    // we can read all the query parameters  using req.query
+    console.log(req.query);
+    res.send({
+        "firstName": "Test First name",
+        "lastName": "Test Last name"
+    })
+});
+
+// request with parameters E.g. /user/101
+app.get("/user/:userId", (req, res) => {
+    // we can read all the parameters  using req.params
+    console.log(req.params);
+    res.send({
+        "firstName": "Test First name",
+        "lastName": "Test Last name"
+    });
 });
 
 
