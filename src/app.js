900aacdbc397196dfc6674b4cb1867d91937ceb5
aishwarya2198 <nuwal.aishwarya06@gmail.com>
2024-10-26 23:59:18 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -3,16 +3,21 @@ const express = require("express");
 // create server
 const app = express();
 
-// create request handlers => has route information along with req and res
+// create request handlers => has route information along with req and res. 
+// app.use() will match all API calls to rote so best practice is app.HTTPMethod()
+app.get("/test", (req, res) => {
+    res.send("GET call")
+})
+app.post("/test", (req, res) => {
+    res.send("POST call")
+})
+app.delete("/test", (req, res) => {
+    res.send("DELETE call")
+})
 app.use("/test", (req, res) => {
     res.send("Hello World from Test");
 });
-app.use("/hello", (req, res) => {
-    res.send("Hello")
-});
-app.use("/", (req, res) => {
-    res.send("Hello World from Dashboard!");
-});
+
 
 // listen to requests to port 3000
 app.listen(3000, () => {
